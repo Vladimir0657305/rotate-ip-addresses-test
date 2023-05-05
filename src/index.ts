5e8e8223fcdc7e96ea5d6a33ffa1ce6eed48274b
Vladimir0657305 <vv0657305@gmail.com>
2023-05-05 20:14:00 +0300

    changes

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,16 @@
-import { JSDOM } from 'jsdom';
 import dotenv from 'dotenv';
 import express from 'express';
 import { rotateWithBrightData } from './rotateWithBrightData';
 
 dotenv.config();
-const jsdom = require("jsdom");
-const { JSDOM } = jsdom;
 
 const app = express();
 const port = 3000;
 
-
+app.get('/', async (req, res) => {
+    const data = await rotateWithBrightData();
+    res.send(data);
+});
 
 app.listen(port, () => {
     console.log(`Server listening at http://localhost:${port}`);
