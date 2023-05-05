f43cbd65047b5bb425c789fe4afd0e94a8ec1523
Vladimir0657305 <vv0657305@gmail.com>
2023-05-06 00:38:37 +0300

    changes

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,120 @@
 import dotenv from 'dotenv';
 import express from 'express';
+import * as parse5 from 'parse5';
 import { rotateWithBrightData } from './rotateWithBrightData';
+const fs = require('fs');
+
+
+import fetch from 'node-fetch';
 
 dotenv.config();
 
 const app = express();
 const port = 3000;
+const parsedData = [];
+const dataExample = 'Hello, World!';
+
+// (async () => {
+//     const url = 'https://www.example.com';
+//     const data = await rotateWithBrightData(url);
+//     console.log(data);
+// })();
 
 app.get('/', async (req, res) => {
-    const data = await rotateWithBrightData();
-    res.send(data);
+    console.log('WORK');
+    try {
+        //     (async () => {
+        //     const url = 'https://www.example.com';
+        //     const data = await rotateWithBrightData(url);
+        //     console.log(data);
+        // })();
+        let page = 0;
+        let url = 'https://www.phin.org.uk/search/consultants?s_location_input=London&s_location_coordinates=51.5072178%2C-0.1275862&s_speciality_input=General%20medicine&s_speciality_id=300';
+        let data = await rotateWithBrightData(url);
+        console.log(data);
+        const hrefs = [];
+        const document = parse5.parse(data);
+
+        const findHref = (node) => {
+            if (node.tagName === 'a' && node.attrs.find((attr) => attr.name === 'class' && attr.value === 'view-button')) {
+                const href = node.attrs.find((attr) => attr.name === 'href');
+                if (href) hrefs.push(href.value);
+            }
+            if (node.childNodes) {
+                node.childNodes.forEach((childNode) => findHref(childNode));
+            }
+        };
+
+        const findData = (node) => {
+            let name = '';
+            let emailHref;
+            let phoneHref;
+            if (node.tagName === 'p' && node.attrs.find((attr) => attr.name === 'id' && attr.value === 'entity-name')) {
+                name = node.childNodes.find((childNode) => childNode.nodeName === '#text').value.trim();
+                console.log('Name:', name);
+            }
+
+            if (node.tagName === 'div' && node.attrs.find((attr) => attr.name === 'id' && attr.value === 'email-button')) {
+                const emailLink = node.querySelector('a') as HTMLAnchorElement;
+                emailHref = emailLink?.getAttribute('href') || '';
+                console.log('Email:', emailHref);
+            }
+
+            if (node.tagName === 'div' && node.attrs.find((attr) => attr.name === 'id' && attr.value === 'call-button')) {
+                const phoneLink = node.querySelector('a') as HTMLAnchorElement;
+                phoneHref = phoneLink?.getAttribute('href') || '';
+                console.log('Phone:', phoneHref);
+            }
+
+            parsedData.push({
+                name: name,
+                email: emailHref,
+                phone: phoneHref,
+            });
+
+
+            if (node.childNodes) {
+                node.childNodes.forEach((childNode) => findData(childNode));
+            }
+        };
+
+
+        findHref(document);
+
+        while (hrefs.length > 0 && page < 2) {
+            page++;
+            const nextPageUrl = url + `&s_page_number=${page}`;
+            const nextUrl = `https://www.phin.org.uk/${hrefs.shift()}`;
+            data = await rotateWithBrightData(nextUrl);
+
+            const nextDocument = parse5.parse(data);
+            findData(nextDocument);
+
+            if (hrefs.length === 0 && page > 0) {
+                url = nextPageUrl;
+                data = await rotateWithBrightData(url);
+                const finalDocument = parse5.parse(data);
+                const finalHrefs = hrefs.slice();
+
+                while (finalHrefs.length > 0) {
+                    const finalUrl = `https://www.phin.org.uk/${finalHrefs.shift()}`;
+                    const finalData = await rotateWithBrightData(finalUrl);
+                    const finalDocument = parse5.parse(finalData);
+                    findData(finalDocument);
+                }
+            }
+        }
+
+        fs.writeFile('file.txt', dataExample, (err) => {
+            console.log('WRITE');
+            if (err) throw err;
+            console.log('Data has been written to file');
+        });
+        res.send('Data parsed successfully');
+    } catch (err) {
+        console.error(err);
+        res.status(500).send('Server error');
+    }
 });
 
 app.listen(port, () => {
