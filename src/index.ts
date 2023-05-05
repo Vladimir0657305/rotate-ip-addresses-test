1403688c28ba0032b166c56dbfa5b177121fcec0
Vladimir0657305 <vv0657305@gmail.com>
2023-05-05 18:45:53 +0300

    changes

--- src/index.ts
+++ src/index.ts
@@ -6,7 +6,7 @@ dotenv.config();
 })();
 
 async function rotateWithBrightData() {
-    const url = 'https://lumtest.com/myip.json';
+    const url = 'https://www.phin.org.uk/search/consultants?s_location_input=London&s_location_coordinates=51.5072178%2C-0.1275862&s_speciality_input=General%20medicine&s_speciality_id=300';
     const rp = require('request-promise');
     const options = {
         url: url,
@@ -14,7 +14,9 @@ async function rotateWithBrightData() {
         rejectUnauthorized: false
     };
     rp(options)
-        .then(function (data) { console.log(data); })
+        .then(function (data) { 
+            // console.log(data); 
+        })
         .catch(function (err) { console.error(err); });
 }
 
