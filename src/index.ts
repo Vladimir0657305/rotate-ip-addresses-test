d566e4fb90e31a309b5f02b7a6224026598bf7ed
Vladimir0657305 <vv0657305@gmail.com>
2023-05-05 20:04:54 +0300

    changes

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,17 @@
 import dotenv from 'dotenv';
+import express from 'express';
+import { rotateWithBrightData } from './rotateWithBrightData';
+
 dotenv.config();
 
-(async () => {
-    await rotateWithBrightData();
-})();
+const app = express();
+const port = 3000;
 
-async function rotateWithBrightData() {
-    const url = 'https://www.phin.org.uk/search/consultants?s_location_input=London&s_location_coordinates=51.5072178%2C-0.1275862&s_speciality_input=General%20medicine&s_speciality_id=300';
-    const rp = require('request-promise');
-    const options = {
-        url: url,
-        proxy: `http://${process.env.luminatiUsername}-session-rand${Math.ceil(Math.random() * 10000000)}:${process.env.luminatiPassword}@zproxy.lum-superproxy.io:22225`,
-        rejectUnauthorized: false
-    };
-    rp(options)
-        .then(function (data) { 
-            // console.log(data); 
-        })
-        .catch(function (err) { console.error(err); });
-}
+app.get('/', async (req, res) => {
+    const data = await rotateWithBrightData();
+    res.send(data);
+});
 
+app.listen(port, () => {
+    console.log(`Server listening at http://localhost:${port}`);
+});
---
+++ src/rotateWithBrightData.ts
@@ -0,0 +1,13 @@
+import rp from 'request-promise';
+
+export async function rotateWithBrightData() {
+    const url = 'https://www.phin.org.uk/search/consultants?s_location_input=London&s_location_coordinates=51.5072178%2C-0.1275862&s_speciality_input=General%20medicine&s_speciality_id=300';
+    const options = {
+        url: url,
+        proxy: `http://${process.env.luminatiUsername}-session-rand${Math.ceil(Math.random() * 10000000)}:${process.env.luminatiPassword}@zproxy.lum-superproxy.io:22225`,
+        rejectUnauthorized: false
+    };
+    const data = await rp(options);
+    console.log(data);
+    return data;
+}
